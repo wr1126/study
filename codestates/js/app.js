4c38a52db07b3a12947d8fed2130fa3532f2d062
sinsin <wr1126@naver.com>
2022-06-14 07:45:44 +0900

    add company slider

--- codestates/js/app.js
+++ codestates/js/app.js
@@ -1,8 +1,10 @@
 const header = document.getElementById("header");
+const outcome = document.querySelector(".contentWrap");
 
+//add header boxShadow when scroll
 window.addEventListener("scroll", () => {
-  const header = document.getElementById("header");
   window.scrollY > 70
     ? (header.style.boxShadow = "0px 5px 5px 0px rgba(200, 200, 200, 0.1)")
     : (header.style.boxShadow = "none");
+  window.scrollY > 3900 ? outcome.classList.add("animated") : null;
 });
