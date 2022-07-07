aa174aaf7f20e70e5798897a8dbea4fb43fcfdf9
sinsin <wr1126@naver.com>
2022-07-07 21:01:47 +0900

    react study

--- tabmenu_react/src/App.js
+++ tabmenu_react/src/App.js
@@ -1,42 +1,49 @@
 import "./App.css";
 import { useState } from "react";
 
-const Tabmenu = () => {
+function App() {
   const tabNames = ["HTML", "CSS", "JS", "REACT"];
-  const tabContents = ["Html is...", "Css is...", "Js is...", "React is..."];
+  const tabContents = {
+    0: "Html is...",
+    1: "Css is...",
+    2: "Js is...",
+    3: "React is...",
+  };
   const [idx, setIdx] = useState("0");
   const [active, setActive] = useState("");
 
   const onMenuClick = (e) => {
     setIdx(e.target.id);
-    setActive("active");
+
+    for (let i = 0; i < tabNames.length; i++) {
+      console.log(tabNames[i].id);
+      if (e.target.id === tabNames[i].id) {
+        setActive("active");
+      } else {
+        setActive("");
+      }
+    }
   };
 
   return (
-    <div id="tabMenu">
-      <div id="menuWrapper">
-        {tabNames.map((name, i) => {
-          return (
-            <div
-              key={name.toString()}
-              id={i}
-              className={`tabName ${active}`}
-              onClick={onMenuClick}
-            >
-              {name}
-            </div>
-          );
-        })}
-      </div>
-      <div id="content">{tabContents[idx]}</div>
-    </div>
-  );
-};
-
-function App() {
-  return (
     <div className="App">
-      <Tabmenu></Tabmenu>
+      <div id="tabMenu">
+        <div id="menuWrapper">
+          {tabNames.map((name, i) => {
+            return (
+              <div
+                key={name.toString()}
+                id={i}
+                className={`tabName ${active === "active" ? "active" : null}`}
+                onClick={onMenuClick}
+              >
+                {name}
+              </div>
+            );
+          })}
+        </div>
+        <div id="content">{tabContents[idx]}</div>
+      </div>
     </div>
   );
 }
